443a0d73d46be46dd7bce3f97fd7005a436e01ad
Jinkyum Park <me@jinkyumpark.com>
2024-05-25 01:26:53 +0900

    [object] add initial project

---
+++ object/src/main/kotlin/com/jinkyumpark/object/ObjectApplication.kt
@@ -0,0 +1,11 @@
+package com.jinkyumpark.object
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ObjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<ObjectApplication>(*args)
+}
